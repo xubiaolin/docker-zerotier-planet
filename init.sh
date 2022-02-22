e8b6002e0669ad6614071bf48552ad1f4606a2ff
root <root@localhost.localdomain>
2022-02-23 01:40:52 +0800

    修复ztncui install的bug

--- init.sh
+++ init.sh
@@ -5,11 +5,9 @@ npm config set registry http://registry.npm.taobao.org && npm install -g node-gy
 
 echo "下载源码中,源码文件较大，请耐心等待;如果源码下载失败，请重新执行该脚本"
 # 下载源码
-cd /opt && git clone http://github.markxu.vip/https://github.com/key-networks/ztncui.git
-if [ "$?"-ne 0]; then echo "下载源码出错，请重试"; exit 1; fi
-
+cd /opt && git clone https://github.markxu.workers.dev/https://github.com/xubiaolin/ztncui.git
 cd /opt && git clone http://github.markxu.vip/https://github.com/zerotier/ZeroTierOne.git
-if [ "$?"-ne 0]; then echo "下载源码出错，请重试"; exit 1; fi
+
 
 # 配置ztncui
 cd /opt/ztncui/src && npm install
