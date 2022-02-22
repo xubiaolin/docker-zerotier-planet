30025606c88f6a673629b068099e6221df36e485
root <root@localhost.localdomain>
2022-02-23 01:49:01 +0800

    fix npm install

--- init.sh
+++ init.sh
@@ -5,15 +5,12 @@ npm config set registry http://registry.npm.taobao.org && npm install -g node-gy
 
 echo "下载源码中,源码文件较大，请耐心等待;如果源码下载失败，请重新执行该脚本"
 # 下载源码
-cd /opt && git clone https://github.markxu.workers.dev/https://github.com/xubiaolin/ztncui.git
+cd /opt && git clone http://github.markxu.vip/https://github.com/xubiaolin/ztncui.git
 cd /opt && git clone http://github.markxu.vip/https://github.com/zerotier/ZeroTierOne.git
 
-
 # 配置ztncui
-cd /opt/ztncui/src && npm install
+cd /opt/ztncui/src && npm install argon2 && npm install
 cp -pv ./etc/default.passwd ./etc/passwd
 echo 'HTTP_PORT=3443' >.env
 echo 'NODE_ENV=production' >>.env
 echo 'HTTP_ALL_INTERFACES=true' >>.env
-
-
