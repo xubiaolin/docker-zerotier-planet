55dad0137c5195932e8e7d0643eebdb8ed07dce9
root <root@localhost.localdomain>
2022-05-14 22:47:25 +0800

    feat: 🎸 替换源

--- init.sh
+++ init.sh
@@ -1,14 +1,15 @@
 echo "update source"
-echo "deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal universe
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates universe
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal multiverse
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates multiverse
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security universe
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security multiverse"> /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+"> /etc/apt/sources.list
 apt update 
 apt upgrade -y
 apt install -y tzdata 
