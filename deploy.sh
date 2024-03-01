30c3d5e75fca39f7282e752aead3a77be015962c
xubiaolin <xubiaolin>
2024-03-01 21:19:35 +0800

    update-kernel

--- deploy.sh
+++ deploy.sh
@@ -14,22 +14,35 @@ kernel_check(){
             exit 1  
         fi
     else
-        # 绿色文字提示
+        echo "当前内核版本为：$kernel_version"
         echo -e "\033[32m系统和内核版本检查通过。\033[0m"
     fi
 }
 
 update_centos_kernal(){
     echo "请注意备份数据，升级内核有风险"
-    # 输入y/Y继续
     read -p "是否继续升级内核?(y/n)" continue_update
     continue_update=${continue_update:-n}
     if [[ "$continue_update" =~ ^[Yy]$ ]]; then
         echo "开始升级内核..."
+        yum update -y
         rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-        rpm -Uvh https://www.elrepo.org/elrepo-release-7.el8.elrepo.noarch.rpm
-        yum --enablerepo=elrepo-kernel install kernel-ml -y
+        yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
+        yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
+        yum --enablerepo=elrepo-kernel install kernel-lt-devel kernel-lt -y
+        sudo awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg
         grub2-set-default 0
+        grub2-mkconfig -o /boot/grub2/grub.cfg
+
+        read -p "内核升级完成，请重启系统，是否立刻重启?(y/n)" reboot
+        reboot=${reboot:-n}
+        if [[ "$reboot" =~ ^[Yy]$ ]]; then
+            reboot
+        else
+            echo "已取消重启"
+            exit 0
+        fi
+
         echo "内核升级完成，请重启系统"
         exit 0
     else
