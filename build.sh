b1c04f9a735776a3f815fae3494d0f779d046031
Markxu <51899048+xubiaolin@users.noreply.github.com>
2024-05-20 14:16:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ DOCKER_IMAGE="xubiaolin/zerotier-planet"
 latest_tag=$(curl -s "https://api.github.com/repos/$USER/$REPO/tags" | jq -r '.[].name' | grep -E "^[0-9]+\.[0-9]+\.[0-9]+$" | sort -V | tail -n 1)
 latest_docker_tag=$(curl -s "https://hub.docker.com/v2/repositories/${DOCKER_IMAGE}/tags/" | jq -r '.results[].name' | grep -E "^[0-9]+\.[0-9]+\.[0-9]+$" | sort -V | tail -n 1)
 
-# if [ "$latest_tag" == "$latest_docker_tag" ]; then
-#     echo "No new version found"
-#     exit 0
-# fi
+if [ "$latest_tag" == "$latest_docker_tag" ]; then
+    echo "No new version found"
+    exit 0
+fi
 
 echo "Latest tag for $USER/$REPO matching latest is: $latest_tag"
 docker buildx build --platform linux/arm64,linux/amd64 -t "$DOCKER_IMAGE":latest --push .
