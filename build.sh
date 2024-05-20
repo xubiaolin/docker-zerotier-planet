acd108f48c92b8d60aa03b6e03ede2631ccdeb17
Markxu <51899048+xubiaolin@users.noreply.github.com>
2024-05-20 09:36:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,4 +14,5 @@ latest_docker_tag=$(curl -s "https://hub.docker.com/v2/repositories/${DOCKER_IMA
 # fi
 
 echo "Latest tag for $USER/$REPO matching latest is: $latest_tag"
-docker buildx build --platform linux/arm64,linux/amd64 -t myimage:latest --push .
+docker buildx build --platform linux/arm64,linux/amd64 -t "$DOCKER_IMAGE":latest --push .
+docker buildx build --platform linux/arm64,linux/amd64 -t "${DOCKER_IMAGE}:${latest_tag}" --push .
