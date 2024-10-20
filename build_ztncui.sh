5134711b48db0e2b7f2656109dacc84211e1aa25
chenxudong2020 <872603935@qq.com>
2024-10-20 19:37:47 +0800

    add

--- build_ztncui.sh
+++ build_ztncui.sh
@@ -7,16 +7,17 @@ export NODEJS_MAJOR=16
 curl -sL -o node_inst.sh https://deb.nodesource.com/setup_${NODEJS_MAJOR}.x
 bash node_inst.sh
 apt install -y nodejs --no-install-recommends
+apt install -y rpm build-essential rpmbuild debhelper fakeroot
+apt install -y rpm alien 
+apt install -y libx32stdc++-12-dev 
 git clone https://github.com/key-networks/ztncui
 npm install -g node-gyp pkg
 
-
-apt install rpm build-essential rpmbuild debhelper fakeroot -y
-apt install libx32stdc++-12-dev -y
-mkdir -p /usr/lib/gcc/x86_64-redhat-linux/8/
-cp /usr/lib/gcc/x86_64-linux-gnu/12/libstdc++fs.a /usr/lib/gcc/x86_64-redhat-linux/8/
-
-sudo apt-get install rubygems
+if [ ! -f /usr/lib/gcc/x86_64-redhat-linux/8/libstdc++fs.a ]; then
+  mkdir -p /usr/lib/gcc/x86_64-redhat-linux/8/
+  cp /usr/lib/gcc/x86_64-linux-gnu/12/libstdc++fs.a /usr/lib/gcc/x86_64-redhat-linux/8/
+fi
+apt install -y rubygems
 gem install fpm
 
 sh zh.sh
